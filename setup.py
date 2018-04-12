5fdbaf0799fbf58a6c1a6864ff811c2782273e12
Michael Bryant <michael.bryant@plangrid.com>
2018-04-12 16:38:43 -0700

    More setup.py and license

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
 from async_http import __version__
@@ -8,4 +7,18 @@ setup(
     name='async-http',
     version=__version__,
     description='Synchronous interface to making asynchronous HTTP requests',
+    author='Michael Bryant',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=[
+        'async_http',
+    ],
+    install_requires=[
+        'aiodns',
+    ],
+    extras_require={
+        'test': ['pytest'],
+    },
 )
